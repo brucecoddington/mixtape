3ac1558dbc033ca977c2a7e1f396b81100f9308f
Bruce Coddington <brucecoddington@gmail.com>
2013-06-29 12:22:10 -0500

    e2e tests working!!!

--- client/specs/e2e/app/controllers/nav.controller.e2e.spec.js
+++ client/specs/e2e/app/controllers/nav.controller.e2e.spec.js
@@ -1,32 +1,18 @@
-define(function(require) {
+(function () {
     'use strict';
 
-    var expect = require('chai').expect;
+    describe('nav.controller', function () {
 
-    var app, controller, scope;
-
-    return (function () {
-
-        describe('nav.controller', function () {
-
-            beforeEach (function () {
-                console.log("here we go!!");
-                browser().navigateTo("http://localhost:8080");
-                sleep(0.5);
-            });
-
-            afterEach (function () {
-                // make sure you clean up any test doubles
-            });
-
-            it('should have the nav controller in the app module', function () {
-                // var link = element('a[href=#/three');
-
-                // link.click();
-                // expect(link.getClass()).to.be.equal('selected');
-
-                console.log('hello');
-            });
+        beforeEach (function () {
+            browser().navigateTo("/");
+            sleep(0.5);
         });
-    }());
-});
+
+        it('should have the nav controller in the app module', function () {
+            var link = element('ul li a:first');
+
+            expect(link).toBeTruthy();
+        });
+    });
+
+}());
--- karma.e2e.config.js
+++ karma.e2e.config.js
@@ -1,12 +1,15 @@
-frameworks = ['mocha', 'requirejs', 'ng-scenario'];
+frameworks = ['ng-scenario'];
 
 files = [
-    "client/assets/js/components/angular-scenario/angular-scenario.js",
-    'client/specs/e2e/**/*.spec.js'
+    "client/specs/e2e/**/*.spec.js"
 ];
 
-singleRun = true;
+proxies = {
+    '/' : 'http://localhost:8080'
+};
+
+urlRoot = "__karma__";
 
 browsers = ['Chrome', 'Firefox'];
 
-plugins = ['karma-mocha', 'karma-requirejs', 'karma-chrome-launcher', 'karma-firefox-launcher', 'karma-ng-scenario'];
+plugins = ['karma-chrome-launcher', 'karma-firefox-launcher', 'karma-ng-scenario'];
